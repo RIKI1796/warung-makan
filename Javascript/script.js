16dfd7a1f6670fd433c6212a2961cb45656a7840
Hafiz17 <119962175+RIKI1796@users.noreply.github.com>
2024-10-22 21:12:45 +0700

    Add files via upload

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,3 @@
-// footer menu
-
 const search_menu = document.getElementById("search-menu");
 const home = document.getElementById("home");
 const home_icon = document.getElementById("home-icon");
@@ -11,7 +9,9 @@ const favorit = document.getElementById("favorit");
 const search = document.getElementById("search");
 const list = document.querySelectorAll(".menu div");
 const sigma = document.getElementById("sigma");
+const footer_menu = document.getElementById("footer-menu");
 
+// fungsi search
 search_icon.addEventListener("click", () => {
   search_menu.style.display = "inherit";
   home.style.display = "none";
@@ -37,7 +37,7 @@ home_icon.addEventListener("click", () => {
 
 user_icon.addEventListener("click", () => {
   search_menu.style.display = "none";
-  home.classList.add("none");
+  home.style.display = "none";
   profil_menu.classList.remove("none");
   daftar_menu.classList.add("none");
   favorit.classList.add("none");
@@ -46,6 +46,7 @@ user_icon.addEventListener("click", () => {
   search_icon.classList.remove("footer-fn");
 });
 
+// fungsi search
 search.addEventListener("input", function () {
   var width = window.innerWidth;
   if (width <= 820) {
@@ -57,6 +58,25 @@ search.addEventListener("input", function () {
         daftar_menu.classList.remove("none");
         item.classList.remove("none");
         item.style.width = "135px"
+      } else {
+        item.classList.add("none");
+        item.classList.remove("show");
+      }
+    });
+  }
+});
+
+search.addEventListener("input", function () {
+  var width = window.innerWidth;
+  if (width >= 1440) {
+    const filter = search.value.toLowerCase();
+    Array.from(list).forEach(function (item) {
+      const text = item.textContent.toLowerCase();
+      if (text.includes(filter)) {
+        item.classList.add("show");
+        daftar_menu.classList.remove("none");
+        item.classList.remove("none");
+        item.style.width = "450px"
       } else {
         item.classList.add("none");
         item.classList.remove("show");
@@ -78,3 +98,17 @@ search.addEventListener("input", () => {
     sigma.classList.add("none");
   }
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var width = window.innerWidth;
+  if (width >= 1440) {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      footer_menu.style.bottom = "0";
+    } else {
+      footer_menu.style.bottom = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
+};
