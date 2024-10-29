3cfca5027a0e258c494b079300851893f0fc7637
Hafiz17 <119962175+RIKI1796@users.noreply.github.com>
2024-10-29 08:49:41 +0700

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -78,6 +78,7 @@ search.addEventListener("input", function () {
       if (text.includes(filter)) {
         item.classList.add("show");
         item.classList.add("showdesk");
+        footer_menu.style.marginTop = "50px";
         daftar_menu.classList.remove("none");
         item.classList.remove("none");
         item.style.width = "300px"
