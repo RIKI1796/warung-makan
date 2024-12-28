f1ddcf1e1208e8bab590dc61620fbb6427aab446
Hafiz17 <119962175+RIKI1796@users.noreply.github.com>
2024-12-28 19:07:12 +0700

    Add files via upload

--- Javascript/script.js
+++ Javascript/script.js
@@ -7,51 +7,47 @@ const profil_menu = document.getElementById("profil-menu");
 const daftar_menu = document.getElementById("daftar-menu");
 const favorit = document.getElementById("favorit");
 const search = document.getElementById("search");
-const list = document.querySelectorAll(".menu div");
+const list = document.querySelectorAll(".daftar-menu .mnuuu");
 const sigma = document.getElementById("sigma");
 const footer_menu = document.getElementById("footer-menu");
 const list2 = document.getElementById("list2");
-const menu = document.getElementById("menu");
+const menu = document.querySelector("#daftar-menu #menu");
 
 // script search
-search_icon.addEventListener("click", () => {
-  search_menu.classList.remove("none");
+search_icon.addEventListener("click", function() {
+  search_menu.style.display = "unset";
   home.style.display = "none";
-  profil_menu.classList.add("none");
   daftar_menu.style.display = "none";
-  favorit.classList.add("none");
-  // footer_menu.style.bottom = "100px";
-  sigma.classList.add("none");
+  favorit.style.display = "none";
+  sigma.style.display = "none";
+  profil_menu.style.display = "none";
   search_icon.classList.add("footer-fn");
   home_icon.classList.remove("footer-fn");
   user_icon.classList.remove("footer-fn");
 });
 
-home_icon.addEventListener("click", () => {
-  search_menu.classList.add("none");
-  home.style.display = "inline";
-  profil_menu.classList.add("none");
-  daftar_menu.style.display = "inline";
-  favorit.classList.remove("none");
-  menu.style.display = "flex";
-  list.style.display = "flex";
-  list2.style.display = "flex";
-  home_icon.classList.add("footer-fn");
+home_icon.addEventListener("click", function() {
+  search_menu.style.display = "none";
+  home.style.display = "unset";
+  daftar_menu.style.display = "unset";
+  favorit.style.display = "inherit";
+  sigma.style.display = "none";
+  profil_menu.style.display = "none";
   search_icon.classList.remove("footer-fn");
+  home_icon.classList.add("footer-fn");
   user_icon.classList.remove("footer-fn");
-  daftar_menu.classList.remove("showdesk");
 });
 
-user_icon.addEventListener("click", () => {
-  search_menu.classList.add("none");
+user_icon.addEventListener("click", function() {
+  search_menu.style.display = "none";
   home.style.display = "none";
-  profil_menu.classList.remove("none")
   daftar_menu.style.display = "none";
-  favorit.classList.add("none");
-  // footer_menu.style.bottom = "0";
-  user_icon.classList.add("footer-fn");
-  home_icon.classList.remove("footer-fn");
+  favorit.style.display = "none";
+  sigma.style.display = "none";
+  profil_menu.style.display = "unset";
   search_icon.classList.remove("footer-fn");
+  home_icon.classList.remove("footer-fn");
+  user_icon.classList.add("footer-fn");
 });
 
 // script search
@@ -62,43 +58,29 @@ search.addEventListener("input", function () {
     Array.from(list).forEach(function (item) {
       const text = item.textContent.toLowerCase();
       if (text.includes(filter)) {
-        item.classList.add("show");
-        item.classList.remove("none");
-        item.style.width = "135px"
+        item.style.display = "unset";
+        daftar_menu.style.display = "unset";
+        item.style.width = "140px";
       } else {
-        item.classList.add("none");
-        item.classList.remove("show");
+        item.style.display = "none";
+        item.style.width = "130px";
       }
     });
   }
 });
 
 search.addEventListener("input", function () {
-  const width = window.innerWidth;
-
-  if (width >= 1360) {
-    const filter = search.value.toLowerCase()
-    const inputValue = search.value.trim().toLowerCase();
+  var width = window.innerWidth;
+  if (width >= 1336) {
+    const filter = search.value.toLowerCase();
     Array.from(list).forEach(function (item) {
       const text = item.textContent.toLowerCase();
-
       if (text.includes(filter)) {
-        item.classList.add("show");
-        menu.style.display = "block";
-        item.style.display = "block";
-        item.classList.add("showdesk");
-        item.style.width = "300px";
+        item.style.display = "unset";
+        daftar_menu.style.display = "unset";
+        item.style.width = "275px";
       } else {
-        item.classList.remove("show");
-        menu.style.dsisplay = "none";
         item.style.display = "none";
-        item.classList.remove("showdesk");
-      }
-       if (inputValue === "") {
-        daftar_menu.style.display = "none"
-        menu.style.display = "none";
-        item.style.display = "none";
-        item.classList.remove("show");
       }
     });
   }
@@ -107,36 +89,64 @@ search.addEventListener("input", function () {
 search.addEventListener("input", () => {
   const inputValue = search.value.trim().toLowerCase();
   const text = ["ad", "add", "add t", "add to", "add to c", "add to ca", "add to car", "add to cart", "id", "idr", "idr.", "idr. "];
-    
-  if (inputValue === "sigma") {
-    daftar_menu.style.display = "none"; 
-    menu.style.display = "none";
-    sigma.classList.remove("none");
-    sigma.classList.add("sigma");
+
+  if (inputValue === "") {
+    daftar_menu.style.display = "none";
+  } else if (inputValue === "sigma") {
+    daftar_menu.style.display = "none";
+    sigma.style.display = "unset";
   } else if (text.includes(inputValue)) {
     daftar_menu.style.display = "none";
-    menu.style.display = "none";
-    sigma.classList.add("none");
+    sigma.style.display = "none";
   } else {
-    daftar_menu.style.display = "inline";
-    menu.style.display = "flex";
-    sigma.classList.add("none");
+    daftar_menu.style.display = "unset";
+    sigma.style.display = "none";
   }
 });
 
-  // var prevScrollpos = window.pageYOffset;
+var tombol = document.querySelector("#profil-menu .button");
+var username = document.querySelector("#profil-menu .username");
+var inputUser = document.createElement('input');
+inputUser.maxLength = 6;
+var confirm = document.querySelector('#profil-menu .confirm');
 
-  // window.onscroll = function() {
-  //   var width = window.innerWidth;
-  //   if (width >= 1440) {
-  //     var currentScrollPos = window.pageYOffset;
+tombol.addEventListener("click", function() {
+  username.replaceWith(inputUser);
+  inputUser.style.transform = 'translateY(7px) translateX(62px)';
+  tombol.replaceWith(confirm);
+  confirm.style.display = 'flex';
+  confirm.style.justifyContent = 'end';
+  confirm.style.transform = 'translateX(0px) translateY(23px)';
+});
 
-  //     if (prevScrollpos < currentScrollPos) {
-  //       footer_menu.style.bottom = "0";
-  //     } else {
-  //       footer_menu.style.bottom = "-100px";
-  //     }
 
-  //     prevScrollpos = currentScrollPos;
-  //   }
-  // };
+confirm.addEventListener("click", function() {
+  if (inputUser.value.trim() === "") {
+    alert("Form tidak boleh kosong");
+  } else {
+    alert("Username Berhasil diganti");
+    const newValue = inputUser.value.trim();
+    inputUser.replaceWith(username);
+    username.textContent = newValue;
+    confirm.replaceWith(tombol);
+  }
+});
+
+var fotoprofil = document.querySelector("#profil-menu .gambar .picture");
+var fileprofil = document.getElementById("picfile");
+var buttonchange = document.getElementById("probutton");
+
+buttonchange.addEventListener('click', function() {
+  fileprofil.click();
+});
+
+fileprofil.addEventListener('change', function() {
+  const file = this.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function(e) {
+    fotoprofil.src = e.target.result;
+  };
+
+  reader.readAsDataURL(file);
+});
